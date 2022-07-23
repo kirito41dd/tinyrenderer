7bd6b1b50f602336a3fbc8d345399c5f9872a19d
kirito <yesiare.cn@gmail.com>
2022-07-23 15:46:28 +0800

    zbuffer

--- src/draw/mod.rs
+++ src/draw/mod.rs
@@ -3,7 +3,7 @@ use std::mem::swap;
 use image::GenericImage;
 
 // 求重心坐标
-fn barycentric(a: glm::IVec2, b: glm::IVec2, c: glm::IVec2, p: glm::IVec2) -> glm::Vec3 {
+fn barycentric(a: glm::Vec3, b: glm::Vec3, c: glm::Vec3, p: glm::Vec3) -> glm::Vec3 {
     let ab = b - a;
     let ac = c - a;
     let pa = a - p;
@@ -13,9 +13,9 @@ fn barycentric(a: glm::IVec2, b: glm::IVec2, c: glm::IVec2, p: glm::IVec2) -> gl
         glm::vec3(ab.y as f32, ac.y as f32, pa.y as f32),
     );
 
-    // 因为传入坐标都是整数，所以z小于1就意味着z是0，这种情况是因为三角形三个顶点在一条直线上，不是合法三角形
+    // z是0，这种情况是因为三角形三个顶点在一条直线上，不是合法三角形
     // 这种情况返回一个负值
-    if u.z.abs() < 1. {
+    if u.z.abs() <= f32::EPSILON {
         return glm::vec3(-1., 1., 1.);
     }
 
@@ -24,24 +24,36 @@ fn barycentric(a: glm::IVec2, b: glm::IVec2, c: glm::IVec2, p: glm::IVec2) -> gl
 }
 
 pub fn triangle<I: GenericImage>(
-    t0: glm::IVec2,
-    t1: glm::IVec2,
-    t2: glm::IVec2,
+    t0: glm::Vec3,
+    t1: glm::Vec3,
+    t2: glm::Vec3,
     image: &mut I,
     color: I::Pixel,
+    zbuffer: &mut [f32],
 ) {
-    let bboxmin = glm::ivec2(t0.x.min(t1.x).min(t2.x), t0.y.min(t1.y).min(t2.y));
-    let bboxmax = glm::ivec2(t0.x.max(t1.x).max(t2.x), t0.y.max(t1.y).max(t2.y));
-    let a = t0[1];
+    let bboxmin = glm::vec2(
+        t0.x.min(t1.x).min(t2.x).max(0.),
+        t0.y.min(t1.y).min(t2.y).max(0.),
+    );
+    let bboxmax = glm::vec2(
+        t0.x.max(t1.x).max(t2.x).min(image.width() as f32 - 1.),
+        t0.y.max(t1.y).max(t2.y).min(image.height() as f32 - 1.),
+    );
 
-    for px in bboxmin.x..=bboxmax.x {
-        for py in bboxmin.y..=bboxmax.y {
-            let bc_screen = barycentric(t0, t1, t2, glm::ivec2(px, py));
+    for px in bboxmin.x as i32..=bboxmax.x as i32 {
+        for py in bboxmin.y as i32..=bboxmax.y as i32 {
+            let bc_screen = barycentric(t0, t1, t2, glm::vec3(px as f32, py as f32, 0.));
 
             if bc_screen.x < 0. || bc_screen.y < 0. || bc_screen.z < 0. {
                 continue;
             }
-            image.put_pixel(px as u32, py as u32, color);
+            // 计算z值
+            let pz = glm::dot(glm::vec3(t0.z, t1.z, t2.z), bc_screen);
+            let idx = px + py * image.width() as i32;
+            if zbuffer[idx as usize] <= pz {
+                zbuffer[idx as usize] = pz;
+                image.put_pixel(px as u32, py as u32, color);
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
+use std::{fs::File, io::BufReader};
 
 use image::{imageops::flip_vertical_in_place, ImageBuffer, Rgba};
 
@@ -12,31 +13,64 @@ const BLUE: Rgba<u8> = Rgba([0, 0, 255, 255]);
 const BLACK: Rgba<u8> = Rgba([0, 0, 0, 255]);
 
 fn main() {
-    let (width, height) = (800, 20);
+    let (width, height) = (800, 800);
     let mut image = ImageBuffer::<Rgba<u8>, _>::from_pixel(width, height, BLACK);
+    let mut zbuffer = vec![f32::MIN; (image.width() * image.height()) as usize]; // 注意一定初始化为最小值
 
-    let mut ybuffer = vec![0; 800];
-    draw::resterize(
-        glm::ivec2(330, 463),
-        glm::ivec2(594, 200),
-        &mut image,
-        &mut ybuffer,
-        BLUE,
-    );
-    draw::resterize(
-        glm::ivec2(120, 434),
-        glm::ivec2(444, 400),
-        &mut image,
-        &mut ybuffer,
-        GREEN,
-    );
-    draw::resterize(
-        glm::ivec2(20, 34),
-        glm::ivec2(744, 400),
-        &mut image,
-        &mut ybuffer,
-        RED,
-    );
+    let input = BufReader::new(File::open("a.obj").unwrap());
+    let model: obj::Obj = obj::load_obj(input).unwrap();
+    let light_dir = glm::vec3(0., 0., -0.9);
+    for arr in model.indices.chunks(3) {
+        let (a, b, c) = (
+            model.vertices.get(arr[0] as usize).unwrap().position,
+            model.vertices.get(arr[1] as usize).unwrap().position,
+            model.vertices.get(arr[2] as usize).unwrap().position,
+        );
+        let (a, b, c) = (
+            glm::vec3(a[0], a[1], a[2]),
+            glm::vec3(b[0], b[1], b[2]),
+            glm::vec3(c[0], c[1], c[2]),
+        );
+        let (sa, sb, sc) = (
+            glm::vec3(
+                ((a.x + 1.) * (width) as f32) / 2. + 0.5,
+                ((a.y + 1.) * (height) as f32) / 2. + 0.5,
+                a.z,
+            ),
+            glm::vec3(
+                ((b.x + 1.) * (width) as f32) / 2. + 0.5,
+                ((b.y + 1.) * (height) as f32) / 2. + 0.5,
+                b.z,
+            ),
+            glm::vec3(
+                ((c.x + 1.) * (width) as f32) / 2. + 0.5,
+                ((c.y + 1.) * (height) as f32) / 2. + 0.5,
+                c.z,
+            ),
+        );
+
+        let n = glm::cross(c - a, b - a);
+        let n = glm::normalize(n);
+
+        let intensity = glm::dot(light_dir, n);
+
+        if intensity > 0. {
+            // 既是光照强度，也能当面剔除用
+            draw::triangle(
+                sa,
+                sb,
+                sc,
+                &mut image,
+                Rgba([
+                    (255. * intensity) as u8,
+                    (255. * intensity) as u8,
+                    (255. * intensity) as u8,
+                    255,
+                ]),
+                &mut zbuffer,
+            );
+        }
+    }
 
     flip_vertical_in_place(&mut image);
     image.save("a.png").unwrap();
